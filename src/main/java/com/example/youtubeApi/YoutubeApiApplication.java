475df45c936bde8321b11da3533e9a45cf08d19f
DESKTOP-46R97AS\User <ggd7chii4@gmail.com>
2019-02-02 12:51:51 +0800

    second commit

---
+++ src/main/java/com/example/youtubeApi/YoutubeApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.youtubeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeApiApplication.class, args);
+	}
+
+}
+
